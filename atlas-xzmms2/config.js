2552d15c99ac9ac7a0ec5a67e179074dd7967842
Evan <evanc.ramsey@gmail.com>
2026-05-09 18:40:59 -0400

    updated google apps script again

--- atlas-xzmms2/config.js
+++ atlas-xzmms2/config.js
@@ -68,7 +68,7 @@ window.CARD_CONFIG = {
     subhead:    "June 8<sup>th</sup> · Washington, DC",
 
     // Where RSVPs go. Paste your Google Apps Script Web App URL here.
-    appsScriptUrl: "https://script.google.com/macros/s/AKfycbyPeOWlT4Gi7AGrNHW3njO968hXThYXr6dW5bWiOx8g2MFZxDVDbPvtua4qaujYNcCs/exec",
+    appsScriptUrl: "https://script.google.com/macros/s/AKfycbzmJtQjDai1nNLDnJkdhz90kzKnmd8ks7tTqI7DzqsEqFq9TB44KjjhFuNlyZ6CfQ5Q/exec",
 
     // Confirmation messages after submission
     confirmYes: {
